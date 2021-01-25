807189b5935c4764f54a75be898f3e590c364b86
mganapatvamsi1 <ganapathiraju.vamsi@gmail.com>
2021-01-25 02:03:31 -0600

    strings problem solving

--- src/main/java/leetcode/arrays/Twelve.java
+++ src/main/java/leetcode/arrays/Twelve.java
@@ -42,6 +42,30 @@ public class Twelve {
         matrix[j][i] = temp;
     }
 
+    //process-1
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        // step-1: transpose
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // swapping first and last
+        for (int i = 0; i < n; i++)  {
+            for (int j = 0; j < n/2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n -  1 - j] = temp;
+            }
+        }
+
+    }
+
+
 
     public static void main(String[] args) {
         Twelve test = new Twelve();
--- src/main/java/leetcode/strings/ReverseString.java
+++ src/main/java/leetcode/strings/ReverseString.java
@@ -3,23 +3,33 @@ package leetcode.strings;
 import java.util.Stack;
 
 public class ReverseString {
+// APPROACH-1
+//    public void reverseString(char[] s) {
+//        char[] reverseArr = new char[s.length];
+//        Stack<Character> stack = new Stack<>();
+//        for (int i = 0; i < s.length; i++) {
+//            System.out.print(stack.push(s[i]));
+//        }
+//        System.out.println();
+//        for (int i = 0; i < s.length; i++) {
+//            Character poppedValue = stack.pop();
+//            reverseArr[i] = poppedValue;
+//            s[i] = reverseArr[i];
+//        }
+//
+//    }
 
+    // APPROACH-2
     public void reverseString(char[] s) {
-        char[] reverseArr = new char[s.length];
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length; i++) {
-            System.out.print(stack.push(s[i]));
+        int start = 0;
+        int end = s.length - 1;
+        while (start < end) {
+            Character temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
         }
-        System.out.println();
-        while (stack.size() != 0) {
-            Character poppedValue = stack.pop();
-            System.out.print(stack.pop());
-        }
-        for (int i = 0; i < s.length; i++) {
-            Character poppedValue = stack.pop();
-            reverseArr[i] = poppedValue;
-        }
-
     }
 
 
